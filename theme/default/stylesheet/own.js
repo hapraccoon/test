abaf4d5b7607eec78623bd09deae92fe104297f5
broenot <bro@vaiver.ru>
2019-11-27 10:33:40 +0400

    deleted max height of product image

--- theme/default/stylesheet/own.js
+++ theme/default/stylesheet/own.js
@@ -45,19 +45,62 @@ function toggleTween(tween) {
 }
 
 
-//CATALOG OPEN
-
-let catalogOpenBtn = document.querySelector('.menu-catalog > .title');
+//MENU MOTION
 let catalogOpenList = document.querySelector('.menu-catalog > ul');
-catalogOpenList.style.display = 'none';
+let swipedMenuBlock = document.querySelector('.swiped');
+let mobileTitle = document.querySelector('.mobile_title-menu');
+let catalogOpenBtn = document.querySelector('.menu-catalog > .title');
+TweenMax.set(swipedMenuBlock, {
+    y: '+=100%',
+    opacity: 0,
+});
+TweenMax.set(catalogOpenList, {
+    position:'fixed',
+    x: 0,
+    opacity: 1
+});
 
+let menuMotion = new TimelineMax()
+    .to(swipedMenuBlock, 0.3, {
+        y: '-=100%',
+        opacity: 1
+    }, 0.1)
+    .to(catalogOpenList, 0.3, {
+        x: '+=100%',
+        opacity: 0
+    }, 0.2)
 
-catalogOpenBtn.addEventListener('click', function () {
-    this.classList.toggle('is_active');
-
-    if (catalogOpenBtn.classList.contains('is_active')) {
-        catalogOpenList.style.display = 'block';
+catalogOpenBtn.addEventListener('click', function (e) {
+    catalogOpenBtn.classList.toggle('an_active');
+    mobileTitle.classList.toggle('closed_nav');
+    menuMotion.reversed(!menuMotion.reversed());
+    if (catalogOpenBtn.classList.contains('an_active')) {
+        // mobileTitle.style.marginLeft = '10px';
     } else {
-        catalogOpenList.style.display = 'none';
+        // mobileTitle.style.marginLeft = '0';
     }
 })
+
+
+//CATALOG OPEN
+
+// let catalogOpenBtn = document.querySelector('.menu-catalog > .title');
+// let catalogOpenList = document.querySelector('.menu-catalog > ul');
+// let swipedMenuBlock = document.querySelector('.swiped');
+// catalogOpenList.style.display = 'none';
+// swipedMenuBlock.style.transform = 'translateX(0)'
+
+
+// catalogOpenBtn.addEventListener('click', function () {
+//     this.classList.toggle('is_active');
+
+//     if (catalogOpenBtn.classList.contains('is_active')) {
+//         catalogOpenList.style.display = 'block';
+//         swipedMenuBlock.style.transform = 'translateX(-100%)'
+//     } else {
+//         catalogOpenList.style.display = 'none';
+//         swipedMenuBlock.style.transform = 'translateX(0)'
+//     }
+// })
+
+//SWIPE MENU
